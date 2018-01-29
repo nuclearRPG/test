b745157325c407b23dd493e306cb59037ebee90f
nuclearRPG <codingjs@163.com>
2018-01-29 12:27:39 +0800

    update dotfiles

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 # update ubuntu
 sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get -y upgrade
 
 # install tools for ubuntu desktop, not necessary for ubuntu server
-sudo apt-get install tmux git vim curl
+sudo apt-get -y install tmux git vim curl
 
 # for remote git repos, not just github
 mkdir -p ~/github
@@ -21,7 +21,7 @@ cat /etc/hosts ~/github/hosts/hosts-files/hosts | sudo tee /etc/hosts
 git clone https://github.com/nuclearRPG/dotfiles ~/github/dotfiles
 ln -s ~/github/dotfiles/vim/.vimrc ~/.vimrc
 ln -s ~/github/dotfiles/js/.eslintrc.json ~/.eslintrc.json
-ln -s ~/github/dotfiles/js/.tern-project ~/.tern-project
+ln -s ~/github/dotfiles/tmux/.tmux.conf ~/.tmux.conf
 
 # vim plugins, use vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
@@ -29,12 +29,18 @@ vim -c 'PluginInstall' -c 'q'
 
 # nvm and node
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
+source ~/.bashrc
 nvm install node
 
 # cnpm, taobao's npm snapshot
 npm i -g npm
 npm i -g cnpm --registry=https://registry.npm.taobao.org
 
+# complete vim plugin youcompleteme
+sudo apt-get -y install python python3 python-dev python3-dev build-essential cmake
+cd ~/.vim/bundle/YouCompleteMe
+./install.py --js-completer
+cd ~
 # eslint, csslint, and eslint-plugin-react
 cnpm i -g eslint csslint eslint-plugin-react
 
